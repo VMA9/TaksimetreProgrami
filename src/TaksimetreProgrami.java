40c87ee05770a0dc416e502a375834c17008a37c
VMA9 <111783796+VMA9@users.noreply.github.com>
2022-09-09 21:37:30 +0300

    Update TaksimetreProgrami.java

--- src/TaksimetreProgrami.java
+++ src/TaksimetreProgrami.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TaksimetreProgrami {
+import java.util.Scanner;
+
+public class TaksimetreProgrami {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = input.nextInt();
+
+        total = km * perKm;
+        total += 10;
+        total = (total < 20) ? 20 : total;
+        System.out.print("Toplam Tutar : " + total);
+
+    }
+
 }
